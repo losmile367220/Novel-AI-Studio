7aa444dcaaf9fc5e67bf48850e5aea1f1afecd88
losmile367220 <losmile367220@gmail.com>
2026-08-06 01:29:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-const APP_CONFIG={GOOGLE_CLIENT_ID:"237913653612-71asrk9ehl4afhotoht1u8v99426seog.apps.googleusercontent.com",GAS_API_URL:"https://script.google.com/macros/s/AKfycbx1_u2dLuOxgrWPG9FrmbgtBRpfKsMW-0rL4VVAbydNc4ZioC02HxyuZxLMToADh__7dw/exec"};
+const APP_CONFIG = {
+  GOOGLE_CLIENT_ID: "237913653612-71asrk9ehl4afhotoht1u8v99426seog.apps.googleusercontent.com",
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbx1_u2dLuOxgrWPG9FrmbgtBRpfKsMW-0rL4VVAbydNc4ZioC02HxyuZxLMToADh__7dw/exec"
+};
